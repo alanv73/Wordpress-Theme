cfa47b4745a1882a6880465d6c45cdb7c3d3e250
Alan Van Art <alanv73@gmail.com>
2020-01-29 08:43:43 -0500

    retype custom header setup

--- functions.php
+++ functions.php
@@ -139,17 +139,17 @@ add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
  * Custom Header
  */
 function uppercase_custom_header_setup() {
-    $args = array(
-        'default-image'      => get_template_directory_uri() . 'assets/images/default-image.jpg',
-        'default-text-color' => '000',
-        'width'              => 1000,
-        'height'             => 250,
-        'flex-width'         => true,
-        'flex-height'        => true,
+	$args = array(
+		'default-image'			=> get_template_directory_uri() . 'assets/images/default-image.jpg',
+		'default-text-color'	=> '000',
+		'width'					=> 1000,
+		'height'				=> 250,
+		'flex-width'			=> true,
+		'flex-height'			=> true,
 	)
-	add_theme_support( 'custom-header', $args );
+	add_theme_support('custom-header', $args);
 }
-add_action( 'after_setup_theme', 'uppercase_custom_header_setup' );
+add_action('after_setup_theme', 'uppercase_custom_header_setup');
 
 /**
  * Implement the Custom Header feature.
