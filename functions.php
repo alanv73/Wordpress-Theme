564e342d0552224d693b0ca742809c8b1e4f88fd
Alan Van Art <alanv73@gmail.com>
2020-01-29 17:32:56 -0500

    change header image size

--- functions.php
+++ functions.php
@@ -96,8 +96,8 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 		add_theme_support( 'custom-header', array(
 			'default-image'			=> get_template_directory_uri() . '/assets/images/default-image.jpg',
 			'default-text-color'	=> '000',
-			'width'					=> 1000,
-			'height'				=> 250,
+			'width'					=> 1280,
+			'height'				=> 320,
 			'flex-width'			=> true,
 			'flex-height'			=> true
 		) );
