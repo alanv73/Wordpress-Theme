bccb693bc71039e5017aafb5f8915394d2d092f0
Alan Van Art <alanv73@gmail.com>
2020-01-29 08:27:51 -0500

    add header image

--- functions.php
+++ functions.php
@@ -136,6 +136,22 @@ function uppercase_scripts() {
 add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
 
 /**
+ * Custom Header
+ */
+function themename_custom_header_setup() {
+    $args = array(
+        'default-image'      => get_template_directory_uri() . 'assets/images/default-image.jpg',
+        'default-text-color' => '000',
+        'width'              => 1000,
+        'height'             => 250,
+        'flex-width'         => true,
+        'flex-height'        => true,
+    )
+    add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
