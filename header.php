5bd5ac5208cb2d88206a061a9d3767fdc9398236
Alan Van Art <alanv73@gmail.com>
2020-01-29 16:04:34 -0500

    header image in saem div as nav

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'uppercase' ); ?></a>
 
+	<div id="site-header">
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
@@ -55,13 +56,13 @@
 		</nav><!-- #site-navigation -->
 		<!-- header image -->
 		<?php if ( get_header_image() ) : ?>
-			<div id="site-header">
+			<!-- <div id="site-header"> -->
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 					<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 				</a>
-			</div>
+			<!-- </div> -->
 		<?php endif; ?>
 		<!-- header image -->
 	</header><!-- #masthead -->
-
+	</div>
 	<div id="content" class="site-content">
