430284a6add6c4f36a40d009dc9db99ee734cffa
Alan Van Art <alanv73@gmail.com>
2020-01-29 08:49:35 -0500

    change array name

--- functions.php
+++ functions.php
@@ -138,18 +138,18 @@ add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
 /**
  * Custom Header
  */
-// function uppercase_custom_header_setup() {
-// 	$args = array(
-// 		'default-image'			=> get_template_directory_uri() . 'assets/images/default-image.jpg',
-// 		'default-text-color'	=> '000',
-// 		'width'					=> 1000,
-// 		'height'				=> 250,
-// 		'flex-width'			=> true,
-// 		'flex-height'			=> true,
-// 	)
-// 	add_theme_support('custom-header', $args);
-// }
-// add_action('after_setup_theme', 'uppercase_custom_header_setup');
+function uppercase_custom_header_setup() {
+	$args2 = array(
+		'default-image'			=> get_template_directory_uri() . 'assets/images/default-image.jpg',
+		'default-text-color'	=> '000',
+		'width'					=> 1000,
+		'height'				=> 250,
+		'flex-width'			=> true,
+		'flex-height'			=> true
+	)
+	add_theme_support('custom-header', $args2);
+}
+add_action('after_setup_theme', 'uppercase_custom_header_setup');
 
 /**
  * Implement the Custom Header feature.
