0919981a43d247644b13002e6906eec92e14dfe9
Alan Van Art <alanv73@gmail.com>
2020-01-29 12:32:53 -0500

    remove args

--- functions.php
+++ functions.php
@@ -140,14 +140,14 @@ add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
  */
 function uppercase_custom_header_setup() {
 	$args = array(
-		// "default-image"			=> get_template_directory_uri() . "assets/images/default-image.jpg",
+		"default-image"			=> get_template_directory_uri() . "assets/images/default-image.jpg",
 		"default-text-color"	=> "000",
 		"width"					=> 1000,
 		"height"				=> 250,
 		"flex-width"			=> true,
 		"flex-height"			=> true
 	)
-	add_theme_support("custom-header", $args);
+	add_theme_support("custom-header");//, $args);
 }
 add_action("after_setup_theme", "uppercase_custom_header_setup");
 
