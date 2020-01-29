a41e8ed309fd76f9ee45e80f4608eaef11d12f8c
Alan Van Art <alanv73@gmail.com>
2020-01-29 08:58:27 -0500

    comment out changes

--- functions.php
+++ functions.php
@@ -82,22 +82,6 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 
 		// Add support for different post formats
 		add_theme_support( 'post-formats',  array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
-
-		/**
-		 * Custom Header
-		 */
-		function uppercase_custom_header_setup() {
-			$args = array(
-				'default-image'			=> get_template_directory_uri() . 'assets/images/default-image.jpg',
-				'default-text-color'	=> '000',
-				'width'					=> 1000,
-				'height'				=> 250,
-				'flex-width'			=> true,
-				'flex-height'			=> true
-			)
-			add_theme_support('custom-header', $args);
-		}
-		// add_action('after_setup_theme', 'uppercase_custom_header_setup');
 	}
 endif;
 add_action( 'after_setup_theme', 'uppercase_setup' );
@@ -150,6 +134,22 @@ function uppercase_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
+
+/**
+ * Custom Header
+ */
+// function uppercase_custom_header_setup() {
+// 	$args2 = array(
+// 		'default-image'			=> get_template_directory_uri() . 'assets/images/default-image.jpg',
+// 		'default-text-color'	=> '000',
+// 		'width'					=> 1000,
+// 		'height'				=> 250,
+// 		'flex-width'			=> true,
+// 		'flex-height'			=> true
+// 	)
+// 	add_theme_support('custom-header', $args2);
+// }
+// add_action('after_setup_theme', 'uppercase_custom_header_setup');
 
 /**
  * Implement the Custom Header feature.
