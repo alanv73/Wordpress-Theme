1ca1d310b6edf5df28ad19e6d78fd3385645d432
Alan Van Art <alanv73@gmail.com>
2020-01-29 12:49:23 -0500

    change defaults to array literal

--- functions.php
+++ functions.php
@@ -84,15 +84,14 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 		add_theme_support( 'post-formats',  array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
 
 		// add theme support for custom header
-		$args = array(
+		add_theme_support( 'custom-header', array(
 			"default-image"			=> get_template_directory_uri() . "assets/images/default-image.jpg",
 			"default-text-color"	=> "000",
 			"width"					=> 1000,
 			"height"				=> 250,
 			"flex-width"			=> true,
 			"flex-height"			=> true
-		)
-		add_theme_support( 'custom-header', $args );
+		) );
 	}
 endif;
 add_action( 'after_setup_theme', 'uppercase_setup' );
