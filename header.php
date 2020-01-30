40cbc181fff23d95f4cdfbc4506a701b53e1a743
Alan Van Art <alanv73@gmail.com>
2020-01-30 07:53:04 -0500

    wrap header and image in div add header css

--- header.php
+++ header.php
@@ -23,32 +23,32 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'uppercase' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<div id="header-content">
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$uppercase_description = get_bloginfo( 'description', 'display' );
-			if ( $uppercase_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $uppercase_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$uppercase_description = get_bloginfo( 'description', 'display' );
+				if ( $uppercase_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $uppercase_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'uppercase' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'uppercase' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', ) ); ?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
 		<!-- header image -->
 		<?php if ( get_header_image() ) : ?>
 			<div id="site-header" class="header-image">
@@ -58,4 +58,5 @@
 			</div>
 		<?php endif; ?>
 		<!-- header image -->
+	</div>
 	<div id="content" class="site-content">
